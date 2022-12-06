1237192fa6ffd5fbd4f01abcda3453f19cef274f
rahul <rk785164@gmail.com>
2022-12-06 14:25:34 +0530

    test cicd

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -75,7 +75,7 @@ const Home = () => {
                     }
                 </Carousel>
 
-            <h2 className="homeHeading">Featured test3</h2>
+            <h2 className="homeHeading">Featured test4</h2>
                 
             {/* Featured Products Container */}
             <div className="container" id="container">
