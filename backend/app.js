bcfa835407e89833075e7603bf23624ee21abf70
rahul <rk785164@gmail.com>
2023-01-06 23:03:06 +0530

    updated api

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,8 @@ require("dotenv").config({path:"backend/config/config.env"});
 
 app.use(express.json());
 app.use(cookieParser())
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload({
     useTempFiles: true,
   }));
